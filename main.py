8d5a834ab295cab85a39d522aee7ab3294bc65e2
James <131554210+Lixvinity@users.noreply.github.com>
2023-04-26 14:49:49 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,32 @@
 import random
 import string
+import copy
 
+Debug = False
 
-NormalCharacters = list(string.ascii_letters + string.digits + string.punctuation)
+Normal_Characters = list(string.ascii_letters + string.digits + string.punctuation)
 
-print (NormalCharacters)
-
-Key = input('Enter Key')
-
+Key = input ('Enter debug key')
 random.seed(Key)
-Output1 = (random.randint())
-print (Output1)
+
+CypherCharacters = copy.deepcopy(Normal_Characters)
+
+random.shuffle(CypherCharacters)
+
+crypt_Type = input('Encrypt or Decrypt... E or D')
+
+if crypt_Type == 'e':
+  crypt_Type = True
+else:
+  crypt_Type = False
+if Debug == True:
+  print (Normal_Characters)
+  print('------NEW LINE------')
+  print (CypherCharacters)
+
+InputPVtext = 'decrypt'
+
+if crypt_Type == True:
+  InputPVtext = ('encrypt')
+
+Text = input('What would you like to ' + InputPVtext)
