fb64213c1b71bb92f8837baba31102de99fbd0c3
James <131554210+Lixvinity@users.noreply.github.com>
2023-05-05 15:08:05 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ import random
 import string
 import copy
 
-Debug = False
 
-Normal_Characters = list(string.ascii_letters + string.digits + string.punctuation)
+
+Debug = True
+
+Normal_Characters = list(string.ascii_letters + string.punctuation + string.digits + 'ĀāĂăĄąĆćĈĉĊċČčĎďĐđĒēĔĕĖėĘęĚěĜĝĞğĠġĢģĤĥĦħĨĩĪīĬĭĮįİıĲĳĴĵĶķĸĹĺĻļĽľĿŀŁłŃńŅņŇňŉŊŋŌōŎŏŐőŒœŔŕŖŗŘřŚśŜŝŞşŠšŢţŤťŦŧŨũŪūŬŭŮůŰűŲųŴŵŶŷŸŹźŻżŽžſƀƁƂƃƄƅƆƇƈƉƊƋƌƍƎƏƐƑƒƓƔƕƖƗƘƙƚƛƜƝƞƟƠơƢƣƤƥƦƧƨƩƪƫƬƭƮƯưƱƲƳƴƵƶƷƸƹƺƻƼƽƾƿǀǁǂǃǄǅǆǇǈǉǊǋǌǍǎǏǐǑǒǓǔǕǖǗǘǙǚǛǜǝǞǟǠǡǢǣǤǥǦǧǨǩǪǫǬǭǮǯǰǱǲǳǴǵǶǷǸǹǺǻǼǽǾǿȀȁȂȃȄȅȆȇȈȉȊȋȌȍȎȏȐȑȒȓȔȕȖȗȘșȚțȜȝȞȟȠȡȢȣȤȥȦȧȨȩȪȫȬȭȮȯȰȱȲȳȴȵȶȷȸȹȺȻȼȽȾȿɀɁɂɃɄɅɆɇɈɉɊɋɌɍɎɏɐɑɒɓɔɕɖɗɘəɚɛɜɝɞɟɠɡɢɣɤɥɦɧɨɩɪɫɬɭɮɯɰɱɲɳɴɵɶɷɸɹɺɻɼɽɾɿʀʁʂʃʄʅʆʇʈʉʊʋʌʍʎʏʐʑʒʓʔʕʖʗʘʙʚʛʜʝʞʟʠʡʢʣʤʥʦʧʨʩʪʫʬʭʮʯʰʱʲʳʴʵʶʷʸʹʺʻʼʽʾʿˀˁ˂˃˄˅ˆˇבגדהוזחטיךכלםמןנסעֈ')
 
 Key = input ('Enter debug key')
 random.seed(Key)
@@ -15,18 +17,33 @@ random.shuffle(CypherCharacters)
 
 crypt_Type = input('Encrypt or Decrypt... E or D')
 
-if crypt_Type == 'e':
+if crypt_Type == 'e' or crypt_Type == 'E':
   crypt_Type = True
-else:
+if crypt_Type == 'd' or crypt_Type == 'D':
   crypt_Type = False
 if Debug == True:
   print (Normal_Characters)
   print('------NEW LINE------')
   print (CypherCharacters)
 
-InputPVtext = 'decrypt'
+
 
 if crypt_Type == True:
-  InputPVtext = ('encrypt')
+  InputPVtext = 'encrypt'
+  Text = input('What would you like to ' + InputPVtext)
+  CipherText = ''
+  for letter in Text:
+   index = Normal_Characters.index(letter)
+   CipherText += CypherCharacters[index]
+  print (CipherText)
 
-Text = input('What would you like to ' + InputPVtext)
+
+if crypt_Type == False:
+  InputPVtext = 'decrypt'
+  CipherText = input('What would you like to ' + InputPVtext)
+  Text = ''
+  for letter in CipherText:
+   index = CypherCharacters.index(letter)
+   Text += Normal_Characters[index]
+  print (Text)
+
